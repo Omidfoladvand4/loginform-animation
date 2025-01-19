bcd3c0af9831a549afc8e55047d206609942694d
omid <frontenddev.omid@gmail.com>
2025-01-19 12:18:07 +0330

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const log = document.querySelector('#login');
+const reg = document.querySelector('#register');
+const btn = document.querySelector('#btn');
+
+function login() {
+    log.style.right = '0'
+    reg.style.right = '450px'
+    btn.style.right = '0'
+}
+function register() {
+ log.style.right = '-450px'
+ reg.style.right = '0'
+ btn.style.right = '110px'
+}
